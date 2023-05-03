bc0508c26d9aa44581a57a551c28623b242c99aa
OBI <77929524+ezimoha-obichukwu@users.noreply.github.com>
2023-05-03 11:40:12 -1200

    update

--- content/urls.py
+++ content/urls.py
@@ -3,12 +3,12 @@ from .views import *
 
 
 urlpatterns = [
-    path('', index_page, name= 'index.html'),
-    path('about.html/', about_page, name= 'about.html'),
-    path('service.html/', service_page, name='service.html'),
-    path('team.html/', team_page, name='team.html'),
-    path('testimonial.html/', testimonial_page, name='testimonial.html'),
-    path('contact.html/', contact_page, name='contact.html'),
-    path('booking.html/', booking_page, name='booking.html'),
-    path('menu.html/', menu_page, name='menu.html')
+    path('', index_page, name= 'index'),
+    path('about/', about_page, name= 'about'),
+    path('service/', service_page, name='service'),
+    path('team/', team_page, name='team'),
+    path('testimonial/', testimonial_page, name='testimonial'),
+    path('contact/', contact_page, name='contact'),
+    path('booking/', booking_page, name='booking'),
+    path('menu/', menu_page, name='menu')
 ]
